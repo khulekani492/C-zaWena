d3974388886c59e1f5ac0ab29108cf9e804b9d78
khulekaniszondo6@gmail.com <khulekaniszondo6@gmail.com>
2026-06-17 18:43:42 +0200

    url end_point for business SIGN UP

---
+++ src/main/java/server/AddBusiness.java
@@ -0,0 +1,55 @@
+package server;
+
+import java.sql.*;
+
+public class AddBusiness {
+    private String Business_name;
+    private String service_type;
+    private String client_name;
+
+
+    public AddBusiness(String businessName, String service_type, String clientName){
+        this.Business_name = businessName;
+        this.service_type = service_type;
+        this.client_name = clientName;
+
+    }
+
+    public  String register_() throws SQLException {
+        //jdbc connection
+        String jdbcUrl = "jdbc:postgresql://aws-0-eu-west-1.pooler.supabase.com:6543/postgres?user=postgres.dklypwwqkhgnqqlzpjrs&password=!4PNfgC-_U6ZjL5";
+        String sql = """
+                 INSERT INTO m_s_s_clients(business_name,service_type,client_name,client_amount) VALUES (?,?,?,?)\s
+                \s""";
+        Connection connection = DriverManager.getConnection(jdbcUrl);
+
+        try (PreparedStatement pstm = connection.prepareStatement(sql)) {
+            pstm.setString(1, this.Business_name);
+            pstm.setString(2, this.service_type);
+            pstm.setString(3, this.client_name);
+            pstm.setInt(4,200);
+
+            pstm.executeUpdate();
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+     return "Success";
+    }
+
+
+     public static void main(String[] args) throws SQLException {
+
+        AddBusiness test_business = new AddBusiness("MKhulex Rental","property","Mdu");
+
+
+
+        try {
+            test_business.register_();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        }
+
+
+}
--- src/main/java/server/c_zaWena.java
+++ src/main/java/server/c_zaWena.java
@@ -2,18 +2,34 @@ package server;
 
 import io.javalin.Javalin;
 
+import java.sql.*;
+import java.util.Map;
+
 public class c_zaWena {
+
     void main() {
         Javalin.create(config -> {
-
             config.bundledPlugins.enableCors(cors -> {
                 cors.addRule(it -> {
                     it.anyHost();
                 });
             });
 
-            config.routes.get("/",ctx ->{
-                ctx.result("Hello: " );
+            config.routes.get("/sign_up",ctx ->{
+                String business_name = ctx.formParam("name");
+                String service_type = ctx.formParam("service");
+                String client_name = ctx.formParam("clientName");
+                Integer amount =  Integer.parseInt(   ctx.formParam("amount"));
+
+                AddBusiness new_business = new AddBusiness(business_name,service_type, client_name);
+                try{
+                    new_business.register_();
+                    ctx.json(Map.of("status", "OK"));
+                } catch (SQLException e) {
+                    ctx.json(Map.of("status", "Failed" ));
+                    throw new RuntimeException(e);
+                }
+
 //                ctx.render("templates/index.html");
             });
             config.routes.get("/login", ctx ->{
